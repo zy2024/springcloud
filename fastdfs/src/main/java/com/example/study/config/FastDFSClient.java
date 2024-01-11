d0691647923376f148ef8d93495391e11d14abc1
zy2024 <1439446606@qq.com>
2024-01-11 15:45:11 +0800

    测试学习fastdfs

---
+++ fastdfs/src/main/java/com/example/study/config/FastDFSClient.java
@@ -0,0 +1,74 @@
+package com.example.study.config;
+
+import org.csource.common.NameValuePair;
+import org.csource.fastdfs.ClientGlobal;
+import org.csource.fastdfs.StorageClient1;
+import org.csource.fastdfs.TrackerClient;
+import org.csource.fastdfs.TrackerServer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+
+@Configuration
+public class FastDFSClient {
+    @Value("${fastdfs.tracker_server}")
+    private String trackerServerIp;
+    @Lazy
+    private StorageClient1 storageClient;
+
+    @Bean
+    public StorageClient1 init() throws Exception {
+        ClientGlobal.init(trackerServerIp);
+        TrackerClient trackerClient = new TrackerClient();
+        TrackerServer trackerServer = trackerClient.getConnection();
+        return new StorageClient1(trackerServer, null);
+    }
+    /**
+     * 上传文件方法
+     * <p>Title: uploadFile</p>
+     * <p>Description: </p>
+     * @param fileName 文件全路径
+     * @param extName 文件扩展名，不包含（.）
+     * @param metas 文件扩展信息
+     * @return
+     * @throws Exception
+     */
+    public String uploadFile(String fileName, String extName, NameValuePair[] metas) throws Exception {
+        String result = storageClient.upload_file1(fileName, extName, metas);
+        return result;
+    }
+
+    public String uploadFile(String fileName) throws Exception {
+        return uploadFile(fileName, null, null);
+    }
+
+    public String uploadFile(String fileName, String extName) throws Exception {
+        return uploadFile(fileName, extName, null);
+    }
+
+    /**
+     * 上传文件方法
+     * <p>Title: uploadFile</p>
+     * <p>Description: </p>
+     * @param fileContent 文件的内容，字节数组
+     * @param extName 文件扩展名
+     * @param metas 文件扩展信息
+     * @return
+     * @throws Exception
+     */
+    public String uploadFile(byte[] fileContent, String extName, NameValuePair[] metas) throws Exception {
+
+        String result = storageClient.upload_file1(fileContent, extName, metas);
+        return result;
+    }
+
+    public String uploadFile(byte[] fileContent) throws Exception {
+        return uploadFile(fileContent, null, null);
+    }
+
+    public String uploadFile(byte[] fileContent, String extName) throws Exception {
+        return uploadFile(fileContent, extName, null);
+    }
+
+}
