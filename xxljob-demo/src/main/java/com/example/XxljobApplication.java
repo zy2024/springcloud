3720aeec5b61487afee29262aba212d03a8483a5
zy2024 <1439446606@qq.com>
2024-03-18 18:56:00 +0800

    xxl-job学习demo

---
+++ xxljob-demo/src/main/java/com/example/XxljobApplication.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxljobApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XxljobApplication.class);
+    }
+}
